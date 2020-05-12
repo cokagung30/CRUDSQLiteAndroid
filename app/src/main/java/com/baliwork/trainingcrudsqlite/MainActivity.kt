ea59162bd3d5bf87670bee0e7a831724e00f2a3c
cokagung30 <cokkagung@gmail.com>
2020-05-12 14:55:17 +0800

    Menampilkan data biodata

--- app/src/main/java/com/baliwork/trainingcrudsqlite/MainActivity.kt
+++ app/src/main/java/com/baliwork/trainingcrudsqlite/MainActivity.kt
@@ -1,23 +1,38 @@
 package com.baliwork.trainingcrudsqlite
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.baliwork.trainingcrudsqlite.adapter.BiodataAdapter
 import com.baliwork.trainingcrudsqlite.sqlite.AppDatabase
 import com.baliwork.trainingcrudsqlite.sqlite.table.Biodata
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
+    private lateinit var biodataAdapter: BiodataAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btn_submit_biodata.setOnClickListener(this)
+
+        showBiodata()
+    }
+
+    private fun showBiodata() {
+        rv_biodata.setHasFixedSize(true)
+        AppDatabase.getInstance().biodataDao().fetchBiodata()
+            .observe(this, Observer { biodatas ->
+                biodataAdapter = BiodataAdapter(biodatas)
+                rv_biodata.adapter = biodataAdapter
+        })
     }
 
     override fun onClick(v: View?) {
-        when(v?.id) {
+        when (v?.id) {
             R.id.btn_submit_biodata -> {
                 val nama = edt_nama.text.toString()
                 val umur = edt_umur.text.toString()
--- app/src/main/java/com/baliwork/trainingcrudsqlite/adapter/BiodataAdapter.kt
+++ app/src/main/java/com/baliwork/trainingcrudsqlite/adapter/BiodataAdapter.kt
@@ -1,21 +1,25 @@
 package com.baliwork.trainingcrudsqlite.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.baliwork.trainingcrudsqlite.R
+import com.baliwork.trainingcrudsqlite.sqlite.table.Biodata
+import kotlinx.android.synthetic.main.item_biodata.view.*
 
-class BiodataAdapter : RecyclerView.Adapter<BiodataAdapter.ViewHolder>() {
+class BiodataAdapter (val biodatas: List<Biodata>) : RecyclerView.Adapter<BiodataAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BiodataAdapter.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_biodata, parent, false))
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount(): Int = biodatas.size
 
     override fun onBindViewHolder(holder: BiodataAdapter.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.itemView.tv_nama.text = biodatas[position].nama
+        holder.itemView.tv_umur.text = biodatas[position].umur.toString()
+        holder.itemView.tv_alamat.text = biodatas[position].alamat
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
