b74a1b3e9927e9a2d9a7889f7a7d553c5aa52b54
cokagung30 <cokkagung@gmail.com>
2020-05-12 14:09:09 +0800

    Update Template Biodata

---
+++ app/src/main/java/com/baliwork/trainingcrudsqlite/adapter/BiodataAdapter.kt
@@ -0,0 +1,24 @@
+package com.baliwork.trainingcrudsqlite.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class BiodataAdapter : RecyclerView.Adapter<BiodataAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BiodataAdapter.ViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: BiodataAdapter.ViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+
+}
